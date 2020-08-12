5c34401547a53d167559a6ec9dcaf1e55517b4f3
leekoko <908230189@qq.com>
2020-08-12 14:10:29 +0800

    同步设计模式

--- SourceCode/defign_pattern/src/main/java/com/geely/design/pattern/creational/builder/v2/Test.java
+++ SourceCode/defign_pattern/src/main/java/com/geely/design/pattern/creational/builder/v2/Test.java
@@ -2,7 +2,11 @@ package com.geely.design.pattern.creational.builder.v2;
 
 public class Test {
     public static void main(String[] args) {
-        Course course = new Course.CourseBuilder().buildCourseName("Java设计模式精讲").buildCoursePPT("Java设计模式精讲PPT").buildCourseVideo("Java设计模式精讲视频").build();
+        Course course = new Course.CourseBuilder()
+                .buildCourseName("Java设计模式精讲")
+                .buildCoursePPT("Java设计模式精讲PPT")
+                .buildCourseVideo("Java设计模式精讲视频")
+                .build();
         System.out.println(course);
     }
 
